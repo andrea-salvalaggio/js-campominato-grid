b6d22f4dc9edd725bd0ef356b8ccd56b35f03e39
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-23 17:05:21 +0200

    bottone play implementato

--- js/script.js
+++ js/script.js
@@ -8,25 +8,6 @@
 // Richiamo il container della griglia tramite il suo id
 const wrapContainer = document.getElementById('container-wrapper');
 
-// Creo un ciclo per generare la griglia
-for (let i = 1; i < 101; i++) {
-    const newSquare = createNewSquare();
-
-    // Aggiungo l'indice (i numeri) dentro al quadrato
-    newSquare.innerHTML = i;
-
-    // Aggiungo il click al quadrato
-    newSquare.addEventListener('click', function () {
-        newSquare.classList.toggle('active');
-
-        // Stampo in console il numero del quadrato cliccato
-        console.log(i);
-    })
-
-    // Aggiungo la griglia al parent
-    wrapContainer.append(newSquare);
-}
-
 // Creo una funzione per generare i quadrati
 function createNewSquare() {
     const currentSquare = document.createElement('div');
@@ -35,5 +16,31 @@ function createNewSquare() {
 }
 
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-const playButton = document.querySelector('play-button');
+const playButton = document.querySelector('.play-button');
+
+// Creo una funzione per generare la griglia (al click del play-button)
+playButton.addEventListener('click', function () {
+
+    // Svuoto il contenitore della griglia
+    wrapContainer.innerHTML = '';
+
+    // Creo un ciclo per generare la griglia
+    for (let i = 1; i < 101; i++) {
+        const newSquare = createNewSquare();
+
+        // Aggiungo l'indice (i numeri) dentro al quadrato
+        newSquare.innerHTML = i;
+
+        // Aggiungo il click al quadrato
+        newSquare.addEventListener('click', function () {
+            newSquare.classList.toggle('active');
+
+            // Stampo in console il numero del quadrato cliccato
+            console.log(i);
+        })
+
+        // Aggiungo la griglia al parent
+        wrapContainer.append(newSquare);
+    }
+})
 
