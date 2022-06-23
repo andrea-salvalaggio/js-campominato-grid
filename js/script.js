8e78e5c7b783082e137ea438758b3b689b91af7e
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-23 16:10:24 +0200

    aggiungo indice numeri e click

--- js/script.js
+++ js/script.js
@@ -8,7 +8,22 @@
 // Richiamo il container della griglia tramite il suo id
 const wrapContainer = document.getElementById('container-wrapper');
 
-// Creo una funzione per generare la griglia
+// Creo un ciclo per generare la griglia
+for (let i = 1; i < 101; i++) {
+    const newSquare = createNewSquare();
+
+    // Aggiungo l'indice (i numeri) dentro al quadrato
+    newSquare.innerHTML = i;
+
+    // Aggiungo il click al quadrato
+    newSquare.addEventListener('click', function () {
+        newSquare.classList.toggle('active');
+    })
+
+    wrapContainer.append(newSquare);
+}
+
+// Creo una funzione per generare i quadrati
 function createNewSquare() {
     const currentSquare = document.createElement('div');
     currentSquare.classList.add('square');
